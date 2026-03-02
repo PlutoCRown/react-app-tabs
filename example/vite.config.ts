96d0023f894233b1468522e8d6f0396a3e0c5d58
jianjun.chen <jianjun.chen@js.design>
2026-03-02 15:35:04 +0800

    修复两份react的问题

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 
+const resolve = (relativePath: string) => new URL(relativePath, import.meta.url).pathname;
+
 export default defineConfig({
   base: '/react-app-tabs/',
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+    alias: {
+      react: resolve('./node_modules/react'),
+      'react-dom': resolve('./node_modules/react-dom'),
+      'react/jsx-runtime': resolve('./node_modules/react/jsx-runtime.js'),
+    },
+  },
   server: {
     port: 5173,
     fs: {
